809b31b157c2ac8071f3a404d49431b1d2f0e76e
Sajjit Shrestha <sajjitshrestha@email.com>
2025-11-02 21:20:34 +0545

    bug fix 2

--- netlify/functions/ably-token.js
+++ netlify/functions/ably-token.js
@@ -1,12 +1,15 @@
-// Netlify function (ESM) that issues an Ably token request
-import Ably from 'ably/promises';
+// netlify/functions/ably-token.js
+const Ably = require('ably/promises');
 
-export async function handler() {
+exports.handler = async function () {
   try {
+    if (!process.env.ABLY_API_KEY) {
+      return { statusCode: 500, body: JSON.stringify({ error: 'Missing ABLY_API_KEY env var' }) };
+    }
     const client = new Ably.Rest(process.env.ABLY_API_KEY);
     const tokenRequest = await client.auth.createTokenRequest({ clientId: 'web' });
     return { statusCode: 200, body: JSON.stringify(tokenRequest) };
   } catch (e) {
-    return { statusCode: 500, body: JSON.stringify({ error: e.message }) };
+    return { statusCode: 500, body: JSON.stringify({ error: e.message, stack: e.stack }) };
   }
-}
+};
